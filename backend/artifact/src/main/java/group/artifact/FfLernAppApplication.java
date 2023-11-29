7ff317e38eef031d0fce552912fa38d869506f11
Richi <richi@hansehart.de>
2023-11-29 21:27:01 +0100

    added: springboot

---
+++ backend/artifact/src/main/java/group/artifact/FfLernAppApplication.java
@@ -0,0 +1,13 @@
+package group.artifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfLernAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfLernAppApplication.class, args);
+	}
+
+}
---
+++ backend/artifact/src/main/java/group/artifact/ServletInitializer.java
@@ -0,0 +1,13 @@
+package group.artifact;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FfLernAppApplication.class);
+	}
+
+}
