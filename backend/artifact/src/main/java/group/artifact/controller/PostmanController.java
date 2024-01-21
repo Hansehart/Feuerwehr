d027048f0a8d5884aa58db3c8a2cfb4586a31312
Richi <richi@hansehart.de>
2024-01-21 21:29:56 +0100

    added: instantiated imagesForVehiclesKey

--- backend/artifact/src/main/java/group/artifact/controller/PostmanController.java
+++ backend/artifact/src/main/java/group/artifact/controller/PostmanController.java
@@ -7,39 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import group.artifact.models.RadioCallSign;
 import group.artifact.repositories.RadioCallSignRepository;
-import group.artifact.models.Image;
-import group.artifact.models.ImagesForVehicles;
-import group.artifact.repositories.ImageRepository;
-import group.artifact.repositories.ImagesForVehiclesRepository;
-
-
 
 @RestController
 public class PostmanController {
     @Autowired
     RadioCallSignRepository radioCallSignRepository;
 
-    @Autowired
-    ImageRepository imageRepository;
-
-    @Autowired
-    ImagesForVehiclesRepository imagesForVehiclesRepository;
-
     @PostMapping("/postman")
     private void postman(@RequestBody RadioCallSign rcs) {
         radioCallSignRepository.save(rcs);
-    }
-
-    @PostMapping("/postman/image")
-    private void postman(@RequestBody Image img) {
-        imageRepository.save(img);
-    }
-
-    @PostMapping("/postman/imageforvehicle")
-    private void postman(@RequestBody ImagesForVehicles imgFVehicles) 
-    {
-        System.out.println("Log imageforvehicle");
-        
-        imagesForVehiclesRepository.save(imgFVehicles);
     }
 }
--- backend/artifact/src/main/java/group/artifact/models/ImagesForVehicles.java
+++ backend/artifact/src/main/java/group/artifact/models/ImagesForVehicles.java
@@ -5,17 +5,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
-// import jakarta.persistence.Table;
-// import lombok.Data;
-// import lombok.NoArgsConstructor;
+import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
+import lombok.Data;
 
-// @Data
+@Data
 @Entity
-// @NoArgsConstructor
-// @Table(name="images_for_vehicles")
+@NoArgsConstructor
+@Table(name="images_for_vehicles")
 public class ImagesForVehicles {
     @EmbeddedId
-    ImagesForVehiclesKey id;
+    ImagesForVehiclesKey id = new ImagesForVehiclesKey();
 
     @ManyToOne
     @MapsId("fkImage")
