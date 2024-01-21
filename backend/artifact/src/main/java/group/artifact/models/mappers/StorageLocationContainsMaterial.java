f37814abf6cd4151f6ea23892de70bc9592f4038
Richi <richi@hansehart.de>
2024-01-21 23:13:03 +0100

    added: StorageLocationContainsMaterial & Key

--- backend/artifact/src/main/java/group/artifact/models/mappers/StorageLocationContainsMaterial.java
+++ backend/artifact/src/main/java/group/artifact/models/mappers/StorageLocationContainsMaterial.java
@@ -1,5 +1,32 @@
 package group.artifact.models.mappers;
 
+import group.artifact.models.Material;
+import group.artifact.models.StorageLocation;
+import group.artifact.models.keys.StorageLocationContainsMaterialKey;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name="storagelocation_contains_material")
 public class StorageLocationContainsMaterial {
-    
+    @EmbeddedId
+    StorageLocationContainsMaterialKey id = new StorageLocationContainsMaterialKey();
+
+    @ManyToOne
+    @MapsId("fkStoragelocation")
+    @JoinColumn(name = "fk_storagelocation")
+    StorageLocation storageLocation;
+
+    @ManyToOne
+    @MapsId("fkMaterial")
+    @JoinColumn(name = "fk_material")
+    Material material;
 }
