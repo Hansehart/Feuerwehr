484581ce6423377401fb6b74431fb316846db388
Richi <richi@hansehart.de>
2024-02-07 18:28:20 +0100

    added: first template component

---
+++ frontend/fflernapp/src/components/ListGroup.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // Hook
+  const [selected, setSelected] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selected === index ? "list-group-item active" : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelected(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
