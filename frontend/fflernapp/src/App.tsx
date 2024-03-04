d1715249f53ae2c2edcbcfa7ae523062fa838d79
Richi <richi@hansehart.de>
2024-03-05 00:10:25 +0100

    added: routing for frontend

--- frontend/fflernapp/src/App.tsx
+++ frontend/fflernapp/src/App.tsx
@@ -1,37 +1,19 @@
-import { useState } from "react";
-import MobileBody from "./components/mobile/MobileBody";
-import MobileHeader from "./components/mobile/MobileHeader";
-import MobileNavBar from "./components/mobile/MobileNavBar";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/Home"; // Assuming you have a Home component
 
 function App() {
-  const [currentView, setCurrentView] = useState("department");
-
-  const changeView = (view: string) => {
-    setCurrentView(view);
-  };
-
-  // decide which body to display
-  let displayComponent;
-  switch (currentView) {
-    case "learn":
-      displayComponent = <MobileBody numberOfCards={5} />;
-      break;
-    case "department":
-      displayComponent = <MobileBody numberOfCards={3} />;
-      break;
-    case "profile":
-      displayComponent = <MobileBody numberOfCards={1} />;
-      break;
-    default:
-      displayComponent = <MobileBody numberOfCards={1} />;
-  }
-
   return (
-    <div>
-      <MobileHeader name="Mollhagen" />
-      {displayComponent}
-      <MobileNavBar changeView={changeView} />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<Navigate replace to="/home" />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/fflernapp/src/pages/Home.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import MobileBody from "../components/mobile/MobileBody";
+import MobileHeader from "../components/mobile/MobileHeader";
+import MobileNavBar from "../components/mobile/MobileNavBar";
+
+function Home() {
+  const [currentView, setCurrentView] = useState("department");
+
+  const changeView = (view: string) => {
+    setCurrentView(view);
+  };
+
+  // decide which body to display
+  let displayComponent;
+  switch (currentView) {
+    case "learn":
+      displayComponent = <MobileBody numberOfCards={5} />;
+      break;
+    case "department":
+      displayComponent = <MobileBody numberOfCards={3} />;
+      break;
+    case "profile":
+      displayComponent = <MobileBody numberOfCards={1} />;
+      break;
+    default:
+      displayComponent = <MobileBody numberOfCards={1} />;
+  }
+
+  return (
+    <div>
+      <MobileHeader name="Mollhagen" />
+      {displayComponent}
+      <MobileNavBar changeView={changeView} />
+    </div>
+  );
+}
+
+export default Home;
