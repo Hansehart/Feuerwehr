9f509225121ffc48ea3e2685654ce25a27592083
Richi <richi@hansehart.de>
2024-03-05 11:51:23 +0100

    added: responsive icons

--- frontend/fflernapp/src/components/RoundedIcon.tsx
+++ frontend/fflernapp/src/components/RoundedIcon.tsx
@@ -46,11 +46,11 @@ export default function RoundedIcon({
       className={"svg-div" + (isActive ? "-clicked" : "")}
       onClick={onClick ? onClick : undefined}
     >
-      <svg width="100" height="100">
+      <svg width="100%" height="100%" viewBox="0 0 100 100">
         <circle
           cx="50"
           cy="50"
-          r="40"
+          r="30"
           fill={bgColor}
           stroke={isActive ? strokeActive : stroke}
           strokeWidth={isActive ? strokeWidthActive : strokeWidth}
