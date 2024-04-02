90beb6813086028d2c768a957e6c3b652bfc3ef1
Richi <richi@hansehart.de>
2024-04-02 14:23:21 +0200

    fixed: required imports added

--- backend/artifact/src/main/java/group/artifact/controller/PostmanController.java
+++ backend/artifact/src/main/java/group/artifact/controller/PostmanController.java
@@ -3,6 +3,7 @@ package group.artifact.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
