0cdc0551c2cc751dfc187e5e9001ac4dd933f289
Richi <richi@hansehart.de>
2024-04-02 11:16:59 +0200

    added: answer changes bordercolor on selection

--- frontend/fflernapp/src/components/mobile/MobileQuizCard.tsx
+++ frontend/fflernapp/src/components/mobile/MobileQuizCard.tsx
@@ -2,6 +2,19 @@ import "./MobileQuizCardStyle.css";
 import drivingFirefighters from "/src/assets/driving-firefighters-filter.jpg";
 
 function MobileQuizCard() {
+  const solution = "answer-1";
+
+  const handleAnswerClick = (answerID: string) => {
+    const solutionElement = document.getElementById(solution);
+    if (solutionElement) {
+      solutionElement.style.borderColor = "green";
+      const selectedElement = document.getElementById(answerID);
+      if (selectedElement && solution != answerID) {
+        selectedElement.style.borderColor = "red";
+      }
+    }
+  };
+
   return (
     <div>
       <section
@@ -14,18 +27,39 @@ function MobileQuizCard() {
         <h3>Einzelauswahl</h3>
       </section>
       <section className="select">
-        <div className="answer">
+        <div
+          className="answer"
+          id="answer-1"
+          onClick={() => handleAnswerClick("answer-1")}
+        >
           <p>50.000</p>
         </div>
-        <div className="answer">
+        <div
+          className="answer"
+          id="answer-2"
+          onClick={() => handleAnswerClick("answer-2")}
+        >
           <p>80.000</p>
         </div>
-        <div className="answer">
+        <div
+          className="answer"
+          id="answer-3"
+          onClick={() => handleAnswerClick("answer-3")}
+        >
           <p>30.000</p>
         </div>
-        <div className="answer">
+        <div
+          className="answer"
+          id="answer-4"
+          onClick={() => handleAnswerClick("answer-4")}
+        >
           <p>100.000</p>
         </div>
+      </section>
+      <section className="continue">
+        <h4 id="timer" style={{ display: "hidden" }}>
+          Weiter in 3...
+        </h4>
       </section>
     </div>
   );
