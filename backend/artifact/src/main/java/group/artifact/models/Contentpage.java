3ee1ae22727be59d7e0f60118fefed1c65d89b69
Richi <fflernapp@hansehart.de>
2024-04-05 16:01:58 +0200

    added: missing attribute subtitle in contentpage

--- backend/artifact/src/main/java/group/artifact/models/Contentpage.java
+++ backend/artifact/src/main/java/group/artifact/models/Contentpage.java
@@ -21,6 +21,7 @@ public class Contentpage {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
+    private String subtitle;
     private String content;
 
     @OneToMany(mappedBy = "contentpage")
