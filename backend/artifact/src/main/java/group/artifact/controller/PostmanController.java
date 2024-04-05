d766f19bb3fa7f8078804f389c9b8496667bbaaf
Richi <fflernapp@hansehart.de>
2024-04-05 10:19:53 +0200

    added: path prefix

--- backend/artifact/src/main/java/group/artifact/controller/PostmanController.java
+++ backend/artifact/src/main/java/group/artifact/controller/PostmanController.java
@@ -60,7 +60,7 @@ public class PostmanController {
         imagesForContentpagesRepository.save(imagesForContentpages);
     }
 
-    @GetMapping("/api/data/hello")
+    @GetMapping("/hello")
     private String hello() {
         return "Hello Test";
     }
