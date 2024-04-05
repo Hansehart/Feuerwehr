50ca8d9267fa6c7b818c03d305654e33fc32ce99
Richi <fflernapp@hansehart.de>
2024-04-05 15:22:08 +0200

    added: controller for saving content pages

---
+++ backend/artifact/src/main/java/group/artifact/controller/ContentpageController.java
@@ -0,0 +1,23 @@
+package group.artifact.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import group.artifact.models.Contentpage;
+import group.artifact.repositories.ContentpageRepository;
+
+@RestController
+@RequestMapping("/api/service")
+public class ContentpageController {
+
+    @Autowired
+    ContentpageRepository contentpageRepository;
+
+    @PostMapping("/save/contentpage")
+    private void postman(@RequestBody Contentpage cp) {
+        contentpageRepository.save(cp);
+    }
+}
