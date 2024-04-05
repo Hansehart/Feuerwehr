015a0f1dc05bd0e353ad9b5543249bebc958bd59
Richi <fflernapp@hansehart.de>
2024-04-05 15:51:55 +0200

    added: test for multiple controllers

--- backend/artifact/src/main/java/group/artifact/controller/ContentpageController.java
+++ backend/artifact/src/main/java/group/artifact/controller/ContentpageController.java
@@ -1,7 +1,6 @@
 package group.artifact.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +15,6 @@ public class ContentpageController {
 
     @Autowired
     ContentpageRepository contentpageRepository;
-
-    @GetMapping("/hello2")
-    public String hello() {
-        return "Hello Test2";
-    }
 
     @PostMapping("/save")
     public void postman(@RequestBody Contentpage cp) {
