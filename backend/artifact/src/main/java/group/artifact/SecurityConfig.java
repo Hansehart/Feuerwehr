b5908c13f9f5bb96c4f5d3085d4c002f9ffed9fd
Richi <fflernapp@hansehart.de>
2024-04-05 09:36:45 +0200

    removed: filter

--- backend/artifact/src/main/java/group/artifact/SecurityConfig.java
+++ backend/artifact/src/main/java/group/artifact/SecurityConfig.java
@@ -4,10 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.FilterChainProxy;
 import org.springframework.security.web.SecurityFilterChain;
-
-import jakarta.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
@@ -16,17 +13,10 @@ public class SecurityConfig {
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
                 return http
-                                .addFilterBefore(requestLoggingFilter(), FilterChainProxy.class)
                                 .authorizeHttpRequests(auth -> auth
-                                                .requestMatchers("/artifact-0.0.1-SNAPSHOT/api/data/**").permitAll()
+                                                .requestMatchers("/artifact-0.0.1-SNAPSHOT/api/data/hello").permitAll()
                                                 .anyRequest().authenticated())
                                 .csrf(req -> req.disable())
                                 .build();
         }
-
-        @Bean
-        public Filter requestLoggingFilter() {
-                return new RequestLoggingFilter();
-        }
-
 }
