c68dc726b59761075d8ebaef3ca320212388eda8
Richi <fflernapp@hansehart.de>
2024-04-09 13:45:18 +0200

    fixed: new algorithm to enable soft hypens

--- frontend/fflernapp/src/components/mobile/MobileBody.tsx
+++ frontend/fflernapp/src/components/mobile/MobileBody.tsx
@@ -29,40 +29,15 @@ export default function MobileBody({
       .catch((error) => console.error("Error fetching data: ", error));
   }, [type]);
 
-  const breakWords = ['lösch', 'gruppen', 'fahrzeug']; // array of words which causes a break
-
-  /*
-  * Provides soft hypen for long words.
-  * i.e title = Löschgruppenfahrzeug, breakWords = [lösch, gruppen, fahrzeug]
-  * then it returns Lösch&shy;gruppen&shy;fahrzeug
-  */
-  const addSoftHyphen = (title: string) => {
-    let modifiedTitle = title;
-    breakWords.forEach(word => {
-      const lowerCaseWord = word.toLowerCase();
-      const index = modifiedTitle.toLowerCase().indexOf(lowerCaseWord);
-      if (index !== -1) { // result found 
-        // check if the first character of the word in the original title was capitalized
-        if (title.charAt(index) === title.charAt(index).toUpperCase()) {
-          word[0].toUpperCase();
-        }
-        modifiedTitle =
-          modifiedTitle.substring(0, index) +
-          word +
-          "\u00AD" + // soft hyphen Unicode character
-          modifiedTitle.substring(index + word.length);
-      }
-    });
-
-    return modifiedTitle;
+  const convertSoftHyphen = (title: string) => {
+    return title.replace(/&shy;/g, "\u00AD");
   };
-
 
   const cards = contentData.map((data, index) => (
     <MobileContentCard
       key={index}
-      title={addSoftHyphen(data.title)}
-      subtitle={addSoftHyphen(data.subtitle)}
+      title={convertSoftHyphen(data.title)}
+      subtitle={convertSoftHyphen(data.subtitle)}
       path={data.path}
     />
   ))
