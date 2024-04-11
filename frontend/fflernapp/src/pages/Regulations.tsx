dcf9a372124dfc28a73225d5b83aa25e6ae17e68
Richi <fflernapp@hansehart.de>
2024-04-11 10:56:31 +0200

    added: instead queries fragments are used for routing

---
+++ frontend/fflernapp/src/pages/Regulations.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import MobileBody from "../components/mobile/MobileBody";
+import MobileHeader from "../components/mobile/MobileHeader";
+import MobileNavBar from "../components/mobile/MobileNavBar";
+
+function Regulations() {
+  const navigate = useNavigate();
+  const [select, setSelect] = useState("");
+
+  useEffect(() => {
+    switch (select) {
+      case "learn":
+        navigate("/home", { state: { select: "learn" } });
+        break;
+      case "department":
+        navigate("/home", { state: { select: "department" } });
+        break;
+      case "profile":
+        navigate("/home", { state: { select: "profile" } });
+        break;
+    }
+  }, [select]);
+
+  const changeView = (view: string) => {
+    setSelect(view);
+  };
+
+  return (
+    <div>
+      <MobileHeader name="Vorschriften" />
+      <MobileBody type=""/>
+      <MobileNavBar changeView={changeView} preset="learn" />
+    </div>
+  );
+}
+
+export default Regulations;
