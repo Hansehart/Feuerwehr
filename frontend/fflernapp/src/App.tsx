4d52a817884815fead31a7392caa925d32d70439
Richi <fflernapp@hansehart.de>
2024-04-11 11:02:02 +0200

    added: home path routing

--- frontend/fflernapp/src/App.tsx
+++ frontend/fflernapp/src/App.tsx
@@ -7,14 +7,16 @@ import {
 import Home from "./pages/Home";
 import Vehicle from "./pages/Vehicle";
 import Exercise from "./pages/Exercise";
+import Regulations from "./pages/Regulations";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/home" element={<Home />} />
-        <Route path="/vehicle" element={<Vehicle />} />
-        <Route path="/learn" element={<Exercise />} />
+        <Route path="/vehicle/view" element={<Vehicle />} />
+        <Route path="/learn/exercise" element={<Exercise />} />
+        <Route path="/learn/regulations" element={<Regulations />} />
         <Route path="*" element={<Navigate replace to="/home" />} />
       </Routes>
     </Router>
