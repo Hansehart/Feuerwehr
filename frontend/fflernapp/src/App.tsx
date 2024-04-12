7efaa03e00b1dc0d4784d40aaea1391932921168
Richi <fflernapp@hansehart.de>
2024-04-12 09:20:22 +0200

    added: page for courses

--- frontend/fflernapp/src/App.tsx
+++ frontend/fflernapp/src/App.tsx
@@ -8,6 +8,7 @@ import Home from "./pages/Home";
 import Vehicle from "./pages/Vehicle";
 import Exercise from "./pages/Exercise";
 import Regulations from "./pages/Regulations";
+import Courses from "./pages/Courses";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/vehicle/view" element={<Vehicle />} />
         <Route path="/learn/exercise" element={<Exercise />} />
         <Route path="/learn/regulations" element={<Regulations />} />
+        <Route path="/learn/courses" element={<Courses />} />
         <Route path="*" element={<Navigate replace to="/home" />} />
       </Routes>
     </Router>
