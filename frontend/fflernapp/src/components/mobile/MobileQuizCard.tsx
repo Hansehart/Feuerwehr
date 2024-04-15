623076427ef441208a697a8881fcceb89eed6137
Richi <fflernapp@hansehart.de>
2024-04-15 14:39:20 +0200

    added: multiple choice

--- frontend/fflernapp/src/components/mobile/MobileQuizCard.tsx
+++ frontend/fflernapp/src/components/mobile/MobileQuizCard.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./MobileQuizCardStyle.css";
 import drivingFirefighters from "/src/assets/driving-firefighters-filter.jpg";
 
@@ -10,12 +10,13 @@ interface QuizData {
 
 function MobileQuizCard() {
   const [count, setCount] = useState(3);
+  const [multipleChoice, setMultipleChoice] = useState<string[]>([]);
   const [timerStarted, setTimerStarted] = useState(false);
   const [quizData, setQuizData] = useState<QuizData | null>(null);
 
   useEffect(() => {
     fetchQuizData();
-  }, [])
+  }, []);
 
   const fetchQuizData = () => {
     setQuizData(null); // clear any data
@@ -38,6 +39,7 @@ function MobileQuizCard() {
           } else {
             clearInterval(countdown); // stop the countdown
             setTimerStarted(false);
+            setMultipleChoice([]);
             setCount(3);
             fetchQuizData();
             return prevCount;
@@ -65,11 +67,47 @@ function MobileQuizCard() {
             // wrong answer selected
             selectedElement.style.borderColor = "red";
           }
+          startTimer();
         }
       } else {
         // multiple choice
+        const solutionIDs = quizData.solutionIndexes.map(
+          (index) => `answer-${index}`
+        );
+        const solutionElements = solutionIDs.map((id) =>
+          document.getElementById(id)
+        );
+        if (solutionElements && !timerStarted) {
+          // element found and timer not started yet (not all answers selected)
+          const selectedElement = document.getElementById(answerID);
+          if (selectedElement) {
+            selectedElement.style.borderColor = "orange";
+            setMultipleChoice([...multipleChoice, answerID]);
+            if (multipleChoice.length === quizData.solutionIndexes.length - 1) {
+              // enough answers selected
+              const selections = Array.from(
+                document.getElementsByClassName("answer")
+              );
+
+              selections.forEach((element) => {
+                // iterate through every answer
+                if (element instanceof HTMLElement) {
+                  if (solutionIDs.includes(element.id)) {
+                    // solution highlighted
+                    element.style.borderColor = "green";
+                    element.style.backgroundColor = "green";
+                    element.style.color = "white";
+                  } else if (multipleChoice.includes(element.id) || element.id === answerID) {
+                    // wrong choice
+                    element.style.borderColor = "red";
+                  }
+                }
+              });
+              startTimer();
+            }
+          }
+        }
       }
-      startTimer();
     }
   };
 
@@ -84,7 +122,11 @@ function MobileQuizCard() {
             <h2>{quizData.text}</h2>
           </section>
           <section className="type">
-            <h3>Einzelauswahl</h3>
+            <h3>
+              {quizData.solutionIndexes.length > 1
+                ? "Mehrfachauswahl"
+                : "Einfachauswahl"}
+            </h3>
           </section>
           <section className="select">
             {quizData.selections.map((selection, index) => (
