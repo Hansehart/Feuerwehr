6370ecded803156272658d26362f9ef85132bc5b
Richi <fflernapp@hansehart.de>
2024-04-16 23:45:54 +0200

    added: basic logic for vehicle

--- frontend/fflernapp/src/pages/Vehicle.tsx
+++ frontend/fflernapp/src/pages/Vehicle.tsx
@@ -5,9 +5,21 @@ import MobileBody from "../components/mobile/MobileBody";
 import MobileHeader from "../components/mobile/MobileHeader";
 import MobileNavBar from "../components/mobile/MobileNavBar";
 
+interface Vehicle {
+  name: string;
+}
+
 function Vehicle() {
   const navigate = useNavigate();
   const [select, setSelect] = useState("");
+  const [vehicle, setVehicle] = useState<Vehicle | null>(null);
+
+  useEffect(() => {
+    fetch(`https://fflernapp.hansehart.de/api/service/receive/vehicle`)
+      .then((response) => response.json())
+      .then((data) => setVehicle(data))
+      .catch((error) => console.error("Error fetching data: ", error));
+  }, []);
 
   useEffect(() => {
     switch (select) {
@@ -29,8 +41,8 @@ function Vehicle() {
 
   return (
     <div>
-      <MobileHeader department={true} name="Mollhagen" />
-      <MobileBody/>
+      <MobileHeader name={vehicle?.name || "Fahrzeug"} />
+      <MobileBody />
       <MobileNavBar changeView={changeView} preset="department" />
     </div>
   );
