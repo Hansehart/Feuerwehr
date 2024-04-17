40d7bcd9d6a3550618906f41ba6b6aa1de6b9c87
Richi <fflernapp@hansehart.de>
2024-04-17 22:00:14 +0200

    fixed: renamed properties in vehicle

--- backend/artifact/src/main/java/group/artifact/dtos/VehicleDTO.java
+++ backend/artifact/src/main/java/group/artifact/dtos/VehicleDTO.java
@@ -8,7 +8,7 @@ public class VehicleDTO {
     // attributes are explained on the example 40-47-01
     private RadioCallSign radioCallSign; // here as location is 40 stored
     private char radioVehicleType; // 47
-    private char radioVehicleNumber; // 01 
+    private char radioVehicleNumber; // 01
+    private String shortcut; 
     private String name;
-    private String type;
 }
