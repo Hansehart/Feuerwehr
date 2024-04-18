8a447594d7e58db1fbed1665bd8e1fea6eaeb662
Richi <fflernapp@hansehart.de>
2024-04-18 14:23:12 +0200

    added: user as entity

---
+++ backend/artifact/src/main/java/group/artifact/models/User.java
@@ -0,0 +1,16 @@
+package group.artifact.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String email;
+    private String password;
+    private String Salt;
+}
