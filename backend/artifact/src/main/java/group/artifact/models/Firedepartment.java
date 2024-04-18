e5806bd737ce59923123f946b9392e6106705acc
Richi <fflernapp@hansehart.de>
2024-04-18 15:44:40 +0200

    fixed: firedepartment marked as entity

--- backend/artifact/src/main/java/group/artifact/models/Firedepartment.java
+++ backend/artifact/src/main/java/group/artifact/models/Firedepartment.java
@@ -1,11 +1,15 @@
 package group.artifact.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name="firedepartment")
 public class Firedepartment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
