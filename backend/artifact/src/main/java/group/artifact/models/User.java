04cfe5f4fb445a5f2acb367b44a08b21bd85ed8f
Richi <fflernapp@hansehart.de>
2024-04-19 08:24:32 +0200

    fixed: user model annotation

--- backend/artifact/src/main/java/group/artifact/models/User.java
+++ backend/artifact/src/main/java/group/artifact/models/User.java
@@ -1,11 +1,15 @@
 package group.artifact.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
