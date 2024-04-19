4ae8e8e70068fbe5043afde7f98c619ffd0708b0
Richi <fflernapp@hansehart.de>
2024-04-19 08:06:15 +0200

    added: user pipe

---
+++ backend/artifact/src/main/java/group/artifact/controller/UserController.java
@@ -0,0 +1,29 @@
+package group.artifact.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import group.artifact.models.User;
+import group.artifact.services.UserService;
+
+@RestController
+@RequestMapping("/api/service")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("/receive/user")
+    public ResponseEntity<String> saveUser(@RequestBody User u) {
+        try {
+            userService.save(u);
+            return ResponseEntity.ok("user successfully created");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        } 
+    }
+}
---
+++ backend/artifact/src/main/java/group/artifact/services/UserService.java
@@ -0,0 +1,33 @@
+package group.artifact.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import group.artifact.models.User;
+import group.artifact.repositories.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public void save(User u) {
+        String hashedPW = DigestUtils.sha256Hex(u.getPassword());
+        u.setPassword(hashedPW);
+        u.setSalt(generateSalt());
+    }
+
+    /*
+     * generates a random string with a length of 10
+     * @return: salt
+     */
+    private String generateSalt() {
+        String randomString = UUID.randomUUID().toString();
+        String salt = DigestUtils.sha256Hex(randomString);
+        return salt.substring(0, 10);
+    }
+}
