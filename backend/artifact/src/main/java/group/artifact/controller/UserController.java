30cd8fccb2a8774376590b1383f91d252c3c9414
Richi <fflernapp@hansehart.de>
2024-04-19 09:20:15 +0200

    fixed: exception handling inserted again

--- backend/artifact/src/main/java/group/artifact/controller/UserController.java
+++ backend/artifact/src/main/java/group/artifact/controller/UserController.java
@@ -1,6 +1,7 @@
 package group.artifact.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +20,11 @@ public class UserController {
 
     @PostMapping("/save/user")
     public ResponseEntity<String> saveUser(@RequestBody User u) {
+        try {
             userService.save(u);
             return ResponseEntity.ok("user successfully created");
-       
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        } 
     }
 }
