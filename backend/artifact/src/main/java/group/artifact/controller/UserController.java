f3deae975b3738e352a50f9ae4fc1000303e250e
Richi <fflernapp@hansehart.de>
2024-04-19 09:06:17 +0200

    fixed: replaced request mapping by post mapping

--- backend/artifact/src/main/java/group/artifact/controller/UserController.java
+++ backend/artifact/src/main/java/group/artifact/controller/UserController.java
@@ -3,6 +3,7 @@ package group.artifact.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @RequestMapping("/receive/user")
+    @PostMapping("/receive/user")
     public ResponseEntity<String> saveUser(@RequestBody User u) {
         try {
             userService.save(u);
