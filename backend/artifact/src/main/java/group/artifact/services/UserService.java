ad37ae2e3cc09142daa3d79dcb890c52370c2d55
Richi <fflernapp@hansehart.de>
2024-04-22 17:58:47 +0200

    added: http only for cookie

--- backend/artifact/src/main/java/group/artifact/services/UserService.java
+++ backend/artifact/src/main/java/group/artifact/services/UserService.java
@@ -24,7 +24,7 @@ public class UserService {
 
         String sid = generateSalt(32);
         Cookie cookie = new Cookie("sid", sid);
-        cookie.setDomain("127.0.0.1");
+        cookie.setHttpOnly(true);
 
         return cookie;
     }
