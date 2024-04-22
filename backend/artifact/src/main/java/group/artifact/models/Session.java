7e0ddcd74a7b1f0a23f06d5f6c702fc676150343
Richi <fflernapp@hansehart.de>
2024-04-23 00:04:49 +0200

    fixed: marked session as entity

--- backend/artifact/src/main/java/group/artifact/models/Session.java
+++ backend/artifact/src/main/java/group/artifact/models/Session.java
@@ -1,11 +1,13 @@
 package group.artifact.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Entity
 @NoArgsConstructor
 public class Session {
     private String sid;
