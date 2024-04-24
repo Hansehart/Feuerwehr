5363b74082e54f9564e0311993bdb644950a5c39
Richi <fflernapp@hansehart.de>
2024-04-24 13:35:47 +0200

    added: space for logout buttn

--- frontend/fflernapp/src/components/mobile/MobileHeader.tsx
+++ frontend/fflernapp/src/components/mobile/MobileHeader.tsx
@@ -8,14 +8,17 @@ interface MobileHeaderProps {
 export default function MobileHeader({ department, name }: MobileHeaderProps) {
   return (
     <header style={{ minHeight: department ? "20vh" : "15vh" }}>
-      {department ? (
-        <h1 data-text={"Feuerwehr " + name}>
-          Feuerwehr<br></br>
-          {name}
-        </h1>
-      ) : (
-        <h1 data-text={name}>{name}</h1>
-      )}
+      <div id="title">
+        {" "}
+        {department ? (
+          <h1 data-text={"Feuerwehr " + name}>
+            Feuerwehr<br></br>
+            {name}
+          </h1>
+        ) : (
+          <h1 data-text={name}>{name}</h1>
+        )}
+      </div>
     </header>
   );
 }
