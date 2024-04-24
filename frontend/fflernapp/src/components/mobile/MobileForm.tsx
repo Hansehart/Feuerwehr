9f8f91fbebaf48ebaa4a8c257c66fc15f2576090
Richi <fflernapp@hansehart.de>
2024-04-24 10:51:11 +0200

    removed: multiple firedepartment selection at once

--- frontend/fflernapp/src/components/mobile/MobileForm.tsx
+++ frontend/fflernapp/src/components/mobile/MobileForm.tsx
@@ -35,7 +35,7 @@ export default function MobileForm({ background, fields }: MobileFormProps) {
             {field.type === "select" ? (
               <>
                 <label htmlFor={`input-${index}`}>{field.label}</label>
-                <select id={`input-${index}`} size={1} multiple>
+                <select id={`input-${index}`} size={1}>
                   {field.selectOptions?.map((option, optionIndex) => (
                     <option key={`option-${index}-${optionIndex}`}>
                       {option}
