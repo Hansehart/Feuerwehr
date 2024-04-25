2de105b19c0e0eeec2415890688f523bccdf0a40
Richi <fflernapp@hansehart.de>
2024-04-25 16:27:25 +0200

    added: renamed fid to firedepartment id

--- backend/artifact/src/main/java/group/artifact/dtos/VehicleDTO.java
+++ backend/artifact/src/main/java/group/artifact/dtos/VehicleDTO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class VehicleDTO {
     // attributes are explained on the example 40-47-01
-    private Integer firedepartmentID; // here as locationNumber is 40 stored
+    private Integer fid; // here as locationNumber is 40 stored
     private String radioVehicleType; // 47
     private String radioVehicleNumber; // 01
     private String shortcut; 
