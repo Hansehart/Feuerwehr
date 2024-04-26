39333a43fce4721262e3dddccf1e9212dbdbf043
Richi <fflernapp@hansehart.de>
2024-04-26 15:40:34 +0200

    added: style for vehicle page

---
+++ frontend/fflernapp/src/components/Table.tsx
@@ -0,0 +1,16 @@
+import "./TableStyle.css";
+
+interface TableProps {
+  header: string[],
+  data: string[]
+}
+
+export default function Table() {
+
+
+  return (
+    <table>
+
+    </table>
+  );
+}
