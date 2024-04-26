00acf93accad37f9984d9d6f66d6ef7ebd935923
Richi <fflernapp@hansehart.de>
2024-04-26 15:58:08 +0200

    added: table component

--- frontend/fflernapp/src/components/Table.tsx
+++ frontend/fflernapp/src/components/Table.tsx
@@ -1,16 +1,23 @@
 import "./TableStyle.css";
 
 interface TableProps {
-  header: string[],
-  data: string[]
+  header: string[];
+  data: string[];
 }
 
-export default function Table() {
+export default function Table({ header, data }: TableProps) {
+  const tableHeader = header.map((h, index) => {
+    return <th key={index}>{h}</th>;
+  });
 
+  const tableData = data.map((d, index) => {
+    return <td key={index}>{d}</td>;
+  });
 
   return (
     <table>
-
+      <tr>{tableHeader}</tr>
+      <tr>{tableData}</tr>
     </table>
   );
 }
