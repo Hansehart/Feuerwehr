bb372ebc6a20bd32995242092ca9a75ef7012fbe
Richi <fflernapp@hansehart.de>
2024-05-06 10:08:41 +0200

    added: request mapping for material controller

--- backend/artifact/src/main/java/group/artifact/controller/MaterialController.java
+++ backend/artifact/src/main/java/group/artifact/controller/MaterialController.java
@@ -5,17 +5,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import group.artifact.dtos.MaterialDTO;
 import group.artifact.services.MaterialService;
 
 @RestController
+@RequestMapping("/api/service")
 public class MaterialController {
     @Autowired
     MaterialService materialService;
 
-    @PostMapping("")
+    @PostMapping("/save/material")
     public ResponseEntity<String> saveMaterial(@RequestBody MaterialDTO m) {
         try {
             materialService.save(m);
