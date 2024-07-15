4a08c30143c0d41edc1183dd27354556de45783b
richi <richi@hansehart.de>
2024-07-15 21:33:06 +0200

    added: textarea as form type

--- frontend/fflernapp/src/components/mobile/basics/MobileForm.tsx
+++ frontend/fflernapp/src/components/mobile/basics/MobileForm.tsx
@@ -3,7 +3,7 @@ import drivingFirefighters from "/src/assets/driving-firefighters-filter.jpg";
 
 interface InputField {
   label?: string; // label element in addition to the input element i. e text before a field for free text
-  type: string;
+  type: string; // choose select, texare or one of the input types
   value?: string; // text inside inputs i. e buttons
   inline?: boolean; // determines if elements should be displayed in a row (against the default: column)
   reverse?: boolean; // true means first input, then label
@@ -16,6 +16,61 @@ interface MobileFormProps {
   background: boolean;
   fields: InputField[];
 }
+
+const renderField = (field: InputField, index: number) => {
+  switch (field.type) {
+    case "select":
+      return (
+        <>
+          <label htmlFor={`input-${index}`}>{field.label}</label>
+          <select id={`input-${index}`} size={1}>
+            {field.selectOptions?.map((option, optionIndex) => (
+              <option key={`option-${index}-${optionIndex}`}>{option}</option>
+            ))}
+          </select>
+        </>
+      );
+
+    case "textarea":
+      return (
+        <>
+          <label htmlFor={`input-${index}`}>{field.label}</label>
+          <textarea
+            id={`input-${index}`}
+          />
+        </>
+      );
+
+    default:
+      return (
+        <>
+          {field.reverse ? (
+            <>
+              <input
+                id={`input-${index}`}
+                type={field.type}
+                value={field.value}
+                onChange={field.onChange}
+                onClick={field.onClick}
+              />
+              <label htmlFor={`input-${index}`}>{field.label}</label>
+            </>
+          ) : (
+            <>
+              <label htmlFor={`input-${index}`}>{field.label}</label>
+              <input
+                id={`input-${index}`}
+                type={field.type}
+                value={field.value}
+                onChange={field.onChange}
+                onClick={field.onClick}
+              />
+            </>
+          )}
+        </>
+      );
+  }
+};
 
 export default function MobileForm({ background, fields }: MobileFormProps) {
   return (
@@ -33,44 +88,7 @@ export default function MobileForm({ background, fields }: MobileFormProps) {
             key={index}
             className={`field-container${field.inline ? "-inline" : ""}`}
           >
-            {field.type === "select" ? (
-              <>
-                <label htmlFor={`input-${index}`}>{field.label}</label>
-                <select id={`input-${index}`} size={1}>
-                  {field.selectOptions?.map((option, optionIndex) => (
-                    <option key={`option-${index}-${optionIndex}`}>
-                      {option}
-                    </option>
-                  ))}
-                </select>
-              </>
-            ) : (
-              <>
-                {field.reverse ? (
-                  <>
-                    <input
-                      id={`input-${index}`}
-                      type={field.type}
-                      value={field.value}
-                      onChange={field.onChange}
-                      onClick={field.onClick}
-                    />
-                    <label htmlFor={`input-${index}`}>{field.label}</label>
-                  </>
-                ) : (
-                  <>
-                    <label htmlFor={`input-${index}`}>{field.label}</label>
-                    <input
-                      id={`input-${index}`}
-                      type={field.type}
-                      value={field.value}
-                      onChange={field.onChange}
-                      onClick={field.onClick}
-                    />
-                  </>
-                )}
-              </>
-            )}
+            {renderField(field, index)}
           </div>
         ))}
       </form>
