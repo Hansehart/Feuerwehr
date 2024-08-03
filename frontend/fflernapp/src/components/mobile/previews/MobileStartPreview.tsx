ea05b51811d7fde0fe399fa901924ccfff84ed5b
richi <richi@hansehart.de>
2024-08-04 00:09:30 +0200

    added: video type

--- frontend/fflernapp/src/components/mobile/previews/MobileStartPreview.tsx
+++ frontend/fflernapp/src/components/mobile/previews/MobileStartPreview.tsx
@@ -5,7 +5,7 @@ export default function MobileStartPreview() {
   return (
     <section>
       <video controls>
-        <source src={extingusihBurningVehicle}/>
+        <source src={extingusihBurningVehicle} type="video/mp4"/>
       </video>
     </section>
   );
