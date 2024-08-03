90ab87f5466324f79a785c53244933ba5996df26
richi <richi@hansehart.de>
2024-08-03 23:40:35 +0200

    fixed: wrong video ending

--- frontend/fflernapp/src/components/mobile/previews/MobileStartPreview.tsx
+++ frontend/fflernapp/src/components/mobile/previews/MobileStartPreview.tsx
@@ -1,5 +1,5 @@
 import "./MobileStartPreviewStyle.css";
-import extingusihBurningVehicle from "/src/assets/videos/extingusih-burning-vehicle.mov";
+import extingusihBurningVehicle from "/src/assets/videos/extingusih-burning-vehicle.mp4";
 
 export default function MobileStartPreview() {
   return (
