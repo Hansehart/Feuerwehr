9349ef0731220090ce4de37e90f09b06f867e734
richi <richi@hansehart.de>
2024-08-04 12:23:39 +0200

    fixed: id naming for video

--- frontend/fflernapp/src/components/mobile/previews/MobileStartPreview.tsx
+++ frontend/fflernapp/src/components/mobile/previews/MobileStartPreview.tsx
@@ -4,8 +4,8 @@ import intro from "/src/assets/videos/intro.mp4";
 export default function MobileStartPreview() {
   return (
     <section id="start-video-section">
-      <video playsInline autoPlay muted loop>
-        <source src={intro} type="video/mp4" id="start-video-intro"/>
+      <video playsInline autoPlay muted loop id="start-video-intro">
+        <source src={intro} type="video/mp4" />
       </video>
       <section id="start-preview-text">
         <h2>Gemeinwohl im Sinn</h2>
