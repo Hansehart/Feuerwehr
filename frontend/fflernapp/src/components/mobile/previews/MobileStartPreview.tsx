91ed83f20b4755fbe5bf262ff5e6b56dc77f4789
richi <richi@hansehart.de>
2024-08-04 12:00:23 +0200

    added: new video file size

--- frontend/fflernapp/src/components/mobile/previews/MobileStartPreview.tsx
+++ frontend/fflernapp/src/components/mobile/previews/MobileStartPreview.tsx
@@ -5,7 +5,7 @@ export default function MobileStartPreview() {
   return (
     <section>
       <video controls>
-        <source src={intro} type="video/webm"/>
+        <source src={intro} type="video/mp4"/>
       </video>
     </section>
   );
