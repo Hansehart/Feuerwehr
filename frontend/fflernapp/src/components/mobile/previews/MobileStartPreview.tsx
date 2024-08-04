c2d530fe9fdc9c6d43000c4abe3934c8c2d35ed3
richi <richi@hansehart.de>
2024-08-04 12:13:59 +0200

    added: text for start preview

--- frontend/fflernapp/src/components/mobile/previews/MobileStartPreview.tsx
+++ frontend/fflernapp/src/components/mobile/previews/MobileStartPreview.tsx
@@ -7,6 +7,10 @@ export default function MobileStartPreview() {
       <video autoPlay muted loop>
         <source src={intro} type="video/mp4" id="video-intro"/>
       </video>
+      <section id="start-preview-text">
+        <p>Gemeinwohl im Sinn</p>
+        <p>Feuer im Herzen</p>
+      </section>
     </section>
   );
 }
