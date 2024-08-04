071547547d1cda865f73f8f86e16d3184882941d
richi <richi@hansehart.de>
2024-08-04 12:08:03 +0200

    added: new video

--- frontend/fflernapp/src/components/mobile/previews/MobileStartPreview.tsx
+++ frontend/fflernapp/src/components/mobile/previews/MobileStartPreview.tsx
@@ -5,7 +5,7 @@ export default function MobileStartPreview() {
   return (
     <section>
       <video autoPlay muted loop>
-        <source src={intro} type="video/mp4"/>
+        <source src={intro} type="video/mp4" id="video-intro"/>
       </video>
     </section>
   );
