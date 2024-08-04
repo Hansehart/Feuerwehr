b9eec81b1f88cfbfa94c438739ce00996f0354fd
richi <richi@hansehart.de>
2024-08-04 12:16:28 +0200

    added: ios support for autoplay videos

--- frontend/fflernapp/src/components/mobile/previews/MobileStartPreview.tsx
+++ frontend/fflernapp/src/components/mobile/previews/MobileStartPreview.tsx
@@ -4,7 +4,7 @@ import intro from "/src/assets/videos/intro.mp4";
 export default function MobileStartPreview() {
   return (
     <section>
-      <video autoPlay muted loop>
+      <video playsInline autoPlay muted loop>
         <source src={intro} type="video/mp4" id="video-intro"/>
       </video>
       <section id="start-preview-text">
