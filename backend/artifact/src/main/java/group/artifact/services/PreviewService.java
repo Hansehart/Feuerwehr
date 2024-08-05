aa14cb39d86b80ebf07afb1a1ba814ce3276f627
Tim Muschke <timluca99@web.de>
2024-08-05 22:39:45 +0200

    Changed: Regulations preview path finding

--- backend/artifact/src/main/java/group/artifact/services/PreviewService.java
+++ backend/artifact/src/main/java/group/artifact/services/PreviewService.java
@@ -1,12 +1,18 @@
 package group.artifact.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ch.qos.logback.core.boolex.Matcher;
 import group.artifact.models.Preview;
 import group.artifact.repositories.PreviewRepository;
+import io.micrometer.common.util.StringUtils;
+
 
 @Service
 public class PreviewService {
@@ -16,13 +22,13 @@ public class PreviewService {
 
     public List<Preview> receive(String type) {
         if (type.equals("learn")) {
-            return previewRepository.findAllByPathStartingWith("/learn%");
+            return reducePreviewResultsToWantedSlashCountHirarchie(previewRepository.findAllByPathStartingWith("/learn%"), "/learn");
         } else if (type.equals("profile")) {
             return previewRepository.findAllByPathStartingWith("/profile%");
         } else if (type.equals("main")) {
             return previewRepository.findAllByPathStartingWith("/main%");
         } else if (type.equals("regulations")) {
-            return previewRepository.findAllByPathStartingWith("/learn/regulations%");
+            return reducePreviewResultsToWantedSlashCountHirarchie(previewRepository.findAllByPathStartingWith("/learn/regulations%"), "/learn/regulations");
         } else {
             System.out.println("ERROR: type unknown in query string when searching for preview");
             return null;
@@ -31,6 +37,33 @@ public class PreviewService {
 
     public void save(Preview p) {
         previewRepository.save(p);
+    }
+
+    private int numberOfSlashes(String str)
+    {
+        return str.length() - str.replace("\\", "").length();
+    }
+
+    private List<Preview> reducePreviewResultsToWantedSlashCountHirarchie(List<Preview> previews, String path)
+    {
+        String myLocalPath = path;
+        if(path.length() == path.lastIndexOf("\\"))
+        {
+            myLocalPath = path.substring(0, path.length()-1);
+        }
+        
+        int numSlashes = numberOfSlashes(myLocalPath);
+        List<Preview> returnPreviews = new ArrayList<>(); //check if needs initialization 
+
+        for (int i = 0; i < previews.size(); i++) 
+        {
+            if(numberOfSlashes(previews.get(0).getPath()) == numSlashes -1 )
+            {
+                returnPreviews.add(previews.get(i));
+            }
+        }
+
+        return returnPreviews;
     }
 
 }
