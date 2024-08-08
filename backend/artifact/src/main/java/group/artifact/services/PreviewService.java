630f9d761f67cfb197542f9234f5c4027a55d9e1
Tim Muschke <timluca99@web.de>
2024-08-08 19:12:25 +0200

    Changed: Regulations preview path finding

--- backend/artifact/src/main/java/group/artifact/services/PreviewService.java
+++ backend/artifact/src/main/java/group/artifact/services/PreviewService.java
@@ -1,17 +1,13 @@
 package group.artifact.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import ch.qos.logback.core.boolex.Matcher;
 import group.artifact.models.Preview;
 import group.artifact.repositories.PreviewRepository;
-import io.micrometer.common.util.StringUtils;
 
 
 @Service
@@ -20,17 +16,9 @@ public class PreviewService {
     @Autowired
     PreviewRepository previewRepository;
 
-    public List<Preview> receive(String type) {
-        if (type.equals("learn")) {
-            // return previewRepository.findAllByPathStartingWith("/learn%");
-            return reducePreviewResultsToWantedSlashCountHirarchie(previewRepository.findAllByPathStartingWith("/learn%"), "/learn");
-        } else if (type.equals("profile")) {
-            return previewRepository.findAllByPathStartingWith("/profile%");
-        } else if (type.equals("main")) {
-            return previewRepository.findAllByPathStartingWith("/main%");
-        } else if (type.equals("regulations")) {
-            return previewRepository.findAllByPathStartingWith("/learn/regulations%");
-            // return reducePreviewResultsToWantedSlashCountHirarchie(previewRepository.findAllByPathStartingWith("/learn/regulations%"), "/learn/regulations");
+    public List<Preview> receive(String path) {
+        if (!path.isEmpty()) {
+            return getPages(previewRepository.findAll(), path);
         } else {
             System.out.println("ERROR: type unknown in query string when searching for preview");
             return null;
@@ -41,43 +29,11 @@ public class PreviewService {
         previewRepository.save(p);
     }
 
-    private int numberOfSlashes(String str)
-    {
-        return str.length() - str.replace("\\", "").length();
-    }
-
-    private List<Preview> reducePreviewResultsToWantedSlashCountHirarchie(List<Preview> previews, String path)
-    {
-        String myLocalPath = path;
-        if(path.length() - 1 == path.lastIndexOf("\\"))
-        {
-            myLocalPath = path.substring(0, path.length()-1);
-        }
-        
-        int numSlashes = numberOfSlashes(myLocalPath);
-
-
-        System.out.println(numSlashes);
-        List<Preview> returnPreviews = new ArrayList<>(); 
-
-        // Preview pr = new Preview();
-        // pr.setId(99);
-        // pr.setPath("/learn");
-        // pr.setSubtitle(path);
-        // pr.setTitle("das ist ein test");
-        // returnPreviews.add(pr);
-        // return returnPreviews;
-
-        for (int i = 0; i < previews.size(); i++) 
-        {
-            if(numberOfSlashes(previews.get(i).getPath()) == numSlashes + 1 )
-            {
-                returnPreviews.add(previews.get(i));
-                System.out.println(previews.get(i).getPath());
-            }
-        }
-
-        return returnPreviews;
+    private List<Preview> getPages(List<Preview> previews, String path) {
+        long baseSlashCount = path.chars().filter(ch -> ch == '/').count();
+        return previews.stream()
+                .filter(preview -> preview.getPath().startsWith(path) && preview.getPath().chars().filter(ch -> ch == '/').count() == baseSlashCount + 1)
+                .collect(Collectors.toList());
     }
 
 }
