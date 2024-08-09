044f4824d58863ea7958b6ecf4b562edf9bc6df2
Tim Muschke <timluca99@web.de>
2024-08-10 01:51:06 +0200

    Added: Map feature first version

--- frontend/fflernapp/src/components/mobile/previews/MobileMapPreview.tsx
+++ frontend/fflernapp/src/components/mobile/previews/MobileMapPreview.tsx
@@ -4,23 +4,8 @@ import "./MobileMapPreviewStyle.css";
 export default function MobileMapPreview() {
   return (
     <section className="department">
-      <div
-        className="department-background"
-      ></div>
-      <div className="department-preview">
-        <div className="department-text">
-          <p>
-            Um eine Wache im Detail sehen zu können,
-            <br />
-            melde dich bitte an.
-          </p>
-
-          <p>
-            Du bist in noch keiner Feuerwehr?
-            <br />
-            Schau direkt mal auf die Karte!
-          </p>
-        </div>
+      <div className="department-background">
+      <iframe width="425" height="350" src="https://www.openstreetmap.org/export/embed.html?bbox=6.6192626953125%2C53.227412682397365%2C11.889953613281252%2C55.22432367289142&amp;layer=mapnik"></iframe><br/><small><a href="https://www.openstreetmap.org/#map=9/54.2379/9.2546">Größere Karte anzeigen</a></small>
       </div>
     </section>
   );
