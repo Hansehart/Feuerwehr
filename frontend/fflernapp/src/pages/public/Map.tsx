5840873c2c6b9dbdb4e217882088fbfefe98de60
Tim Muschke <timluca99@web.de>
2024-08-10 01:43:00 +0200

    Added: Map feature first version

--- frontend/fflernapp/src/pages/public/Map.tsx
+++ frontend/fflernapp/src/pages/public/Map.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import MobileBody from "../../components/mobile/basics/MobileBody";
 import MobileHeader from "../../components/mobile/basics/MobileHeader";
-// import MobileNavBar from "../../components/mobile/basics/MobileNavBar";
+import MobileNavBar from "../../components/mobile/basics/MobileNavBar";
 // import Maintenance from "../../components/general/Maintenance";
 import MobileMapPreview from "../../components/mobile/previews/MobileMapPreview";
 
@@ -35,7 +35,7 @@ function Map() {
       <h3>*** Karte ***</h3>
       {/* <iframe width="425" height="350" src="https://www.openstreetmap.org/export/embed.html?bbox=6.6192626953125%2C53.227412682397365%2C11.889953613281252%2C55.22432367289142&amp;layer=mapnik"></iframe><br/><small><a href="https://www.openstreetmap.org/#map=9/54.2379/9.2546">Größere Karte anzeigen</a></small> */}
       <MobileBody main={<MobileMapPreview/>} marginToFooter="15vh"/>
-      {/* <MobileNavBar changeView={changeView} preset="department" /> */}
+      <MobileNavBar changeView={changeView} preset="department" />
     </div>
   );
 }
