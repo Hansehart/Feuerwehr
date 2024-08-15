fc859228ebad0ac2ed2576d695d229f9a1c63818
richi <richi@hansehart.de>
2024-08-15 21:10:42 +0200

    added: new pic for vehicle page

--- frontend/artifact/src/components/mobile/basics/MobileContentCard.tsx
+++ frontend/artifact/src/components/mobile/basics/MobileContentCard.tsx
@@ -10,7 +10,6 @@ import mechanicPortrait from "/src/assets/pictures/mechanic-portrait.jpg";
 import serviceRegulation from "/src/assets/pictures/service-regulation.jpg";
 import firefighterTeaching from "/src/assets/pictures/firefighter-teaching.jpg";
 import questionmarkPlaceholder from "/src/assets/pictures/questionmark-placeholder.jpg";
-
 import { useState } from "react";
 
 interface MobileContentCardProps {
@@ -29,44 +28,47 @@ export default function MobileContentCard({
   const [option, setOption] = useState("");
   const navigate = useNavigate();
 
-  const handleClick = () => {
+  const handeClick = () => {
     navigate(path);
   };
 
-  // determine the background image and option style
-  const backgroundImg = (() => {
-    switch (img) {
-      case "vehicle":
-        setOption("center");
-        return vehicle;
-      case "serviceCenter":
-        return serviceCenter;
-      case "emergencyExit":
-        return emergencyExit;
-      case "highwayAccident":
-        return highwayAccident;
-      case "mechanicPortrait":
-        return mechanicPortrait;
-      case "exercisePortrait":
-        return exercisePortrait;
-      case "serviceRegulation":
-        return serviceRegulation;
-      case "firefighterTeaching":
-        return firefighterTeaching;
-      default: 
-        return questionmarkPlaceholder;
-    }
-  })();
+  // Determine the background image
+  let backgroundImg;
+  switch (img) {
+    case "vehicle":
+      backgroundImg = vehicle;
+      setOption("center");
+      break;
+    case "serviceCenter":
+      backgroundImg = serviceCenter;
+      break;
+    case "emergencyExit":
+      backgroundImg = emergencyExit;
+      break;
+    case "highwayAccident":
+      backgroundImg = highwayAccident;
+      break;
+    case "mechanicPortrait":
+      backgroundImg = mechanicPortrait;
+      break;
+    case "exercisePortrait":
+      backgroundImg = exercisePortrait;
+      break;
+    case "serviceRegulation":
+      backgroundImg = serviceRegulation;
+      break;
+    case "firefighterTeaching":
+      backgroundImg = firefighterTeaching;
+      break;
+    default: // tewmplate
+      backgroundImg = questionmarkPlaceholder;
+  }
 
   return (
-    <section className="content-card-section" onClick={handleClick}>
+    <section className="content-card-section" onClick={handeClick}>
       <div
         id="content-card-background-image"
-        className=""
-        style={{
-          backgroundImage: `url(${backgroundImg})`,
-          backgroundPositionX: option, // apply the option style here if set
-        }}
+        style={{ backgroundImage: `url(${backgroundImg})`, backgroundPositionX: option }}
       ></div>
       <h1>{title}</h1>
       <h2>{subtitle}</h2>
