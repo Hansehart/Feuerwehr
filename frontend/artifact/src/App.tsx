813fd5d0e5864383d0d6bf78ea57241738b133dd
richi <richi@hansehart.de>
2024-08-17 15:27:00 +0200

    added: paths for routing map and gtc

--- frontend/artifact/src/App.tsx
+++ frontend/artifact/src/App.tsx
@@ -76,7 +76,9 @@ function App() {
           </>
         )}
         <Route path="/start" element={<Start />} />
-        <Route path="/main/map" element={<Map />} />
+        <Route path="/info/map" element={<Construction />} />
+        <Route path="/dev/info/map" element={<Map />} />
+        <Route path="/info/gtc" element={<Construction />} />
 
         <Route path="/learn/exercises" element={<Exercise />} />
         <Route path="/learn/regulations" element={<Construction />} />
