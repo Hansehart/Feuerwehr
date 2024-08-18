9c3eba4dd9160169edd652574a9a4c2e8d66eaaa
Richi <97880342+hbrsRichi@users.noreply.github.com>
2024-08-18 06:57:46 +0200

    Update MobileInfoFooter.tsx

--- frontend/artifact/src/components/mobile/basics/MobileInfoFooter.tsx
+++ frontend/artifact/src/components/mobile/basics/MobileInfoFooter.tsx
@@ -2,15 +2,15 @@ export default function MobileInfoFooter() {
   return (
     <section className="mb-[15vh]">
       <div className="flex justify-evenly items-center h-[15vh]">
-        <a href="https://feuerwehr.hansehart.de/info/gtc" target="_blank">
+        <a href="https://feuerwehr.hansehart.de/info/gtc">
           AGB
         </a>
         <p>|</p>
-        <a href="https://feuerwehr.hansehart.de/info/imprint" target="_blank">
+        <a href="https://feuerwehr.hansehart.de/info/imprint">
           Impressum
         </a>
         <p>|</p>
-        <a href="https://feuerwehr.hansehart.de/info/contact" target="_blank">
+        <a href="https://feuerwehr.hansehart.de/info/contact">
           Kontakt
         </a>
       </div>
