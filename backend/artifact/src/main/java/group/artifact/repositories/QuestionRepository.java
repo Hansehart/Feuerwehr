ae46a635d0e15d28ca64811cc98f38d8ddaacfff
richi <richard.hartung@smail.inf.h-brs.de>
2024-09-28 19:32:35 +0200

    fixed: table name

--- backend/artifact/src/main/java/group/artifact/repositories/QuestionRepository.java
+++ backend/artifact/src/main/java/group/artifact/repositories/QuestionRepository.java
@@ -9,9 +9,9 @@ import group.artifact.models.Question;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
-    @Query(value = "SELECT * FROM questions WHERE category = :category ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM question WHERE category = :category ORDER BY RAND() LIMIT 1", nativeQuery = true)
     Question findRandomByCategory(@Param("category") String category);
 
-    @Query(value = "SELECT * FROM questions ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM question ORDER BY RAND() LIMIT 1", nativeQuery = true)
     Question findRandomQuestion();
 }
