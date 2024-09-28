5de6851315a2b4e5fae19fa6dd365373c89f18a2
richi <richard.hartung@smail.inf.h-brs.de>
2024-09-28 17:53:12 +0200

    add: cards responsive

--- frontend/artifact/src/components/mobile/basics/MobileBody.tsx
+++ frontend/artifact/src/components/mobile/basics/MobileBody.tsx
@@ -84,7 +84,7 @@ export default function MobileBody({
       ) : (
         <>
           {before}
-          <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-center min-h-[60vh] bg-gray-100 rounded-xl p-8 m-8">
+          <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-center min-h-[60vh] bg-gray-100 rounded-xl p-4 m-8">
             {cards.length > 0 ? cards : main}
           </div>
           {after}
