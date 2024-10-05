0d557c66dcbba9cbb4ab2857818de0d072eb6dfe
richi <richard.hartung@smail.inf.h-brs.de>
2024-10-05 19:36:52 +0200

    add: new content card style

--- frontend/artifact/src/components/mobile/basics/MobileBody.tsx
+++ frontend/artifact/src/components/mobile/basics/MobileBody.tsx
@@ -48,7 +48,7 @@ export default function MobileBody({
         data.map((item) => ({
           title: item.shortcut,
           subtitle: item.name,
-          alignment: "items-start",
+          alignment: "items-start pl-1",
           path: `/main/vehicle/${item.radioVehicleType}/${item.radioVehicleNumber}`,
           img: "vehicle",
         }));
